f429cc6f894b2f63507ac7313e762a2d76776d1d
holic345 <holic345@naver.com>
2025-11-08 17:42:24 +0900

    카프카 연동 완료

--- src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -1,12 +1,26 @@
 package com.example.paymentservice;
 
+import java.util.function.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @EnableScheduling
 @SpringBootApplication
 public class PaymentServiceApplication {
+
+    @Bean
+    Function<Flux<Message<String>>, Mono<Void>> consume() {
+        return messageFlux -> messageFlux.flatMap(message -> {
+                System.out.println("processing = " + message.getPayload());
+                return Mono.empty();
+            })
+            .then();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
