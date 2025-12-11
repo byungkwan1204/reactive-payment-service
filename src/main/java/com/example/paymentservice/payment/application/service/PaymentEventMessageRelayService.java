282f328126b177ff2acfad722d82de9f7ccb23dd
holic345 <holic345@naver.com>
2025-12-11 13:40:14 +0900

    [feat] 카프카 토픽에 발송을 실패하거나, 아직 발송하지 않은 메세지들을 전송하는 Relay 테스트 구현

--- src/main/java/com/example/paymentservice/payment/application/service/PaymentEventMessageRelayService.java
+++ src/main/java/com/example/paymentservice/payment/application/service/PaymentEventMessageRelayService.java
@@ -6,14 +6,14 @@ import com.example.paymentservice.payment.application.port.in.PaymentEventMessag
 import com.example.paymentservice.payment.application.port.out.DispatchEventMessagePort;
 import com.example.paymentservice.payment.application.port.out.LoadPendingPaymentEventMessagePort;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.StringUtils;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
-@Profile("dev")
+//@Profile("dev")
 @Usecase
 @RequiredArgsConstructor
 public class PaymentEventMessageRelayService implements PaymentEventMessageRelayUsecase {
@@ -23,12 +23,21 @@ public class PaymentEventMessageRelayService implements PaymentEventMessageRelay
 
     private static final Scheduler scheduler = Schedulers.newSingle("message-relay");
 
+    // 재진입 방지 플래그
+    private final AtomicBoolean running = new AtomicBoolean(false);
+
     @Scheduled(fixedDelay = 100, initialDelay = 100, timeUnit = TimeUnit.SECONDS)
     @Override
     public void relay() {
+
+        if (running.getAndSet(true)) {
+            return;
+        }
+
         loadPendingPaymentEventMessagePort.getPendingPaymentEventMessage()
             .doOnNext(dispatchEventMessagePort::dispatch)
             .onErrorContinue((err, ignore) -> LoggerUtil.error("messageRelay", StringUtils.hasText(err.getMessage()) ? "failed to relay message" : "", err))
+            .doFinally(signal -> running.set(false))
             .subscribeOn(scheduler)
             .subscribe();
     }
